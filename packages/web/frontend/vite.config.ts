d52afadc8f23d4e750a5cde45b592754cd9b50c6
Constant <constantsuchet@gmail.com>
2025-03-09 09:15:27 +0100

    fixed web build

--- packages/web/frontend/vite.config.ts
+++ packages/web/frontend/vite.config.ts
@@ -1,16 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+      '@': resolve(__dirname, './src')
+    }
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
