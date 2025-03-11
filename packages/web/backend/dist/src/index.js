95444c03aa8a8f01f3220407c1eaf3c6cf6dbad8
Constant <constantsuchet@gmail.com>
2025-03-11 17:55:53 +0100

    fixed eslint

--- packages/web/backend/dist/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const port = process.env.PORT || 3005;
-const app = (0, server_1.createServer)();
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
