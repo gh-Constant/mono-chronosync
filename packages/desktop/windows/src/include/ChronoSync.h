0c0bbe24f6d0bc29d2ed77aad26e3c19600af066
CDevs <constantsuchet@gmail.com>
2025-03-21 18:27:53 +0100

    removed windows vers

--- packages/desktop/windows/src/include/ChronoSync.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHRONOSYNC
-#define CHRONOSYNC
-
-#define APP_NAME "ChronoSync"
-
-#endif // CHRONOSYNC
--- packages/desktop/windows/src/include/tracker.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRACKER_H
-#define TRACKER_H
-
-#include "trackerWindow.h"
-#include "trackerAFK.h"
-#include "trackerLogger.h"
-#include "trackerDevice.h"
-
-#endif // TRACKER_H
