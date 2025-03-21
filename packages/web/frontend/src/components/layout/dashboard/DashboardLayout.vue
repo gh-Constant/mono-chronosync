df82fe3e1d4ac19d2270917a1cd0e69afeb8a2de
Constant <constantsuchet@gmail.com>
2025-03-21 07:29:41 +0100

    fixed problems about the sidebar

---
+++ packages/web/frontend/src/components/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { SidebarProvider } from '@/components/ui/sidebar'
+import DashboardSidebar from './DashboardSidebar.vue'
+import DashboardContent from './DashboardContent.vue'
+</script>
+
+<template>
+  <SidebarProvider>
+    <div class="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <DashboardSidebar />
+      <DashboardContent>
+        <slot />
+      </DashboardContent>
+    </div>
+  </SidebarProvider>
+</template> 
